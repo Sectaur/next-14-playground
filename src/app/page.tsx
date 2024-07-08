51632f0db1a0d31958905efa997878b5258eeb3b
Hament Pandya <hament@sectaur.org>
2024-07-08 14:24:56 +1200

    combined purchase modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import PurchaseModal from "./purchaseModal";
+import CombinedPurchaseModal from "./combinedPurchaseModal";
 import TermsAndConditions from "./termsAndConditions";
 
 export default function Home() {
@@ -10,15 +10,16 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <h1 className="text-4xl font-bold mb-8">Welcome to My Course</h1>
+      <h1 className="text-4xl font-bold mb-8">Welcome to My Course</h1>
 
       <Button onClick={() => setIsModalOpen(true)}>Open Purchase Modal</Button>
 
-      <PurchaseModal
+      <CombinedPurchaseModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-      /> */}
-      <TermsAndConditions />
+        userName={"Hament"}
+      />
+      {/* <TermsAndConditions /> */}
     </main>
   );
 }
