b6103bd4b5d19f734bc036f388fbe06b89456856
Hament Pandya <hament@sectaur.org>
2024-07-16 16:18:36 +1200

    slide presenter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ import CombinedPurchaseModal from "./combinedPurchaseModal";
 import TermsAndConditions from "./termsAndConditions";
 import KahootTiradsSCalculator from "./KahootTiradsCalculator";
 import KahootTIRADSCalculator from "./KahootTiradsCalculator";
+import SlidePresenter from "./slidePresenter";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <KahootTIRADSCalculator />
+      <SlidePresenter />
+      {/* <KahootTIRADSCalculator /> */}
       {/* <h1 className="text-4xl font-bold mb-8">Welcome to My Course</h1>
 
       <Button onClick={() => setIsModalOpen(true)}>Open Purchase Modal</Button>
