bff548b009f499f151c419c13e35ddef6d6eb26d
Hament Pandya <hament@sectaur.org>
2024-08-14 17:14:11 +0530

    tidy up tirads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import MockedTIRADSCalculator from "./components/mockedTIRADSCalculator";
+import TIRADSCalculator from "./components/TIRADSCalculator";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <SlidePresenter /> */}
-      <MockedTIRADSCalculator />
+      <TIRADSCalculator />
       {/* <h1 className="text-4xl font-bold mb-8">Welcome to My Course</h1>
 
       <Button onClick={() => setIsModalOpen(true)}>Open Purchase Modal</Button>
