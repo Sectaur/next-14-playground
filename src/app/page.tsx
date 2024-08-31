4aa2e8848acf3057c501ba74b53ff6a7d77e20ba
Hament Pandya <hament@sectaur.org>
2024-08-31 12:16:01 +1200

    removed p-24

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <ThyroidRadiologyLabDashboard />
       {/* <SlidePresenter /> */}
       {/* <TIRADSLabeler /> */}
