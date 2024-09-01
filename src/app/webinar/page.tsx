15dc0c5f4d677afe0e4a1a1e73ca477f7079665f
Hament Pandya <hament@sectaur.org>
2024-09-02 07:34:02 +1200

    webinar page

---
+++ src/app/webinar/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <h1 className="text-white">Webinar Page</h1>
+    </div>
+  );
+}
