b8e97522598adac4f61e582a256cc4615a822627
Hament Pandya <hament@sectaur.org>
2024-09-02 13:02:19 +1200

    delted sinlge scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,7 @@ import WebinarExample from "./pages/webinarExample";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <WebinarExample />
-      {/* <LandingPageExample /> */}
-      {/* <VideosLandingPageExample /> */}
-      {/* <HeroCarouselExample /> */}
+      <LandingPageExample />
     </main>
   );
 }
